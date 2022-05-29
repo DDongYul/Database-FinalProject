54f8573172a1597c00af5f8e18231bbb255ce14c
jslee99 <junsub_lee@naver.com>
2022-05-29 14:02:37 +0900

    test

--- database/v1/v1.py
+++ database/v1/v1.py
@@ -1,6 +1,17 @@
+import pymysql
 
-def crawling():
+
+def open_db():
+    conn = pymysql.connect(host='localhost', user='js_movie', password='jgtmapm3876', db='movie')
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    return conn, cur
+    
+def close_db(conn, cur):
+    cur.close()
+    conn.close()
+    
+def main():
     url = 'https://movie.naver.com/movie/sdb/browsing/bmovie_year.naver'
 
 if __name__ == '__main__':
-    crawling()
+    main()
