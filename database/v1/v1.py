d3c9b1078dbed711499f609ba0c1ec09ae32954d
jslee99 <junsub_lee@naver.com>
2022-05-29 14:31:28 +0900

    test

--- database/v1/v1.py
+++ database/v1/v1.py
@@ -1,5 +1,8 @@
 import pymysql
+from selenium import webdriver
 
+
+URL = 'https://movie.naver.com/movie/sdb/browsing/bmovie_year.naver'
 
 def open_db():
     conn = pymysql.connect(host='localhost', user='js_movie', password='jgtmapm3876', db='movie')
@@ -11,7 +14,12 @@ def close_db(conn, cur):
     conn.close()
     
 def main():
-    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie_year.naver'
-
+    browser = webdriver.Chrome(executable_path='C:\Users\junsub\공부\2022 1학기\데이터베이스\과제#4\src\database\v1')
+    browser.get(url=URL)
+    box = browser.find_elements_by_class_name('directory_item_other > tr > td > a')
+    for e in box:
+        print(e.text)
+    
+    
 if __name__ == '__main__':
     main()
