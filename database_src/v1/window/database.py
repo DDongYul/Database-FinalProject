4fa41187e989243a76e2a375ea6123388cfc4a2c
DDongYul <comeme0101@naver.com>
2022-06-10 03:18:52 +0900

    제목검색기능 , 영화상세정보

---
+++ database_src/v1/window/database.py
@@ -0,0 +1,133 @@
+import pymysql
+
+
+def open_db():
+    conn = pymysql.connect(host='localhost', user='root',
+                           password='rltkd3306!', db='naver_movie', unix_socket='/tmp/mysql.sock')
+
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    return conn, cur
+
+
+def close_db(conn, cur):
+    cur.close()
+    conn.close()
+
+def get_allMovie():
+    conn1, cur1 = open_db()
+    sql = """
+        select movie_id,title
+        from movie;
+        """
+    cur1.execute(sql)
+    movie_title_list = []
+    r = cur1.fetchone()
+    while r:
+        movie_title_list.append([r['movie_id'],r['title']])
+        r=cur1.fetchone()
+    return movie_title_list
+
+def getidWithTitle(title):      #영화 제목으로 id 받아옴
+    conn1, cur1 = open_db()
+    sql = """
+    select movie_id
+    from movie
+    where title = "{0}";
+    """.format(title)
+    cur1.execute(sql)
+    r = cur1.fetchone()
+    id = r['movie_id']
+    return id
+
+def getAllDataWithId(id):       #해당 영화에 나오는 데이터를 movie 테이블에서 전부 가쟈옴
+    conn1, cur1 = open_db()
+    sql = """
+        select *
+        from movie
+        where movie_id = '{0}';
+        """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchall()
+    return r
+
+def getACtorIdWithId(id):       #해당 영화에 나오는 배우들의 act_id를 전부 가져옴
+    conn1, cur1 = open_db()
+    sql = """
+            select act_id
+            from movie_actor
+            where movie_id = '{0}';
+            """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchall()
+    return r
+
+def getActorNameWithActId(id):  #act_id에 해당하는 배우의 이름 조회
+    conn1, cur1 = open_db()
+    sql = """
+                select act_name
+                from actor
+                where act_id = '{0}';
+                """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchall()
+    return r
+
+def getDirectorIdWithId(id):    #해당 영화에 출연하는 감독의 dir_id를 가져옴
+    conn1, cur1 = open_db()
+    sql = """
+                    select dir_id
+                    from movie_director
+                    where movie_id = '{0}';
+                    """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchone()
+    return r
+
+def getDirectorNameWithDirId(id):
+    conn1, cur1 = open_db()
+    sql = """
+                    select dir_name
+                    from director
+                    where dir_id = '{0}';
+                    """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchone()
+    return r
+
+def getDirIdwithDirName(name):
+    conn1, cur1 = open_db()
+    sql = """
+                        select dir_id
+                        from director
+                        where dir_name = '{0}';
+                        """.format(name)
+    cur1.execute(sql)
+    r = cur1.fetchone()
+    return r
+
+def print_Movie(id):
+    conn1, cur1 = open_db()
+    sql = """
+            select *
+            from movie
+            where movie_id = {0};
+            """.format(id)
+    cur1.execute(sql)
+    r = cur1.fetchall()
+    return r
+
+    close_db(conn1, cur1)
+# def get_actor():
+#     conn1, cur1 = open_db()
+#     sql = """
+#     select *
+#     from movie
+#     where movie_id = '174830';
+#     """
+#     cur1.execute(sql)
+#     r= cur1.fetchone()
+#
+#     close_db(conn1,cur1)
+#     print(r)
+
--- database_src/v1/window/main.py
+++ database_src/v1/window/main.py
@@ -1,5 +1,6 @@
 import sys
 import searcher
+import database as db
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5 import QtCore,QtWidgets
@@ -17,8 +18,8 @@ class WindowClass(QMainWindow,form_class ):
         self.sortRateButton.clicked.connect(self.sortRateButtonClick)
         self.tempButton.clicked.connect(self.screenChangeToMovieInfo)
 
-    def screenChangeToMovieInfo(self):
-        self.window_movieinfo = WindowClassMovieInfo()
+    def screenChangeToMovieInfo(self,id):
+        self.window_movieinfo = WindowClassMovieInfo(id)
         self.window_movieinfo.exec_()
 
     #검색버튼 눌렀을 시 실행할 함수
@@ -36,7 +37,13 @@ class WindowClass(QMainWindow,form_class ):
     # 레디오버튼 상태에 따라 검색함수 바뀌게
     def groupboxRadFunction(self) :
         if self.radioButtonTitle.isChecked() :
-            searcher.search(1, self.lineEdit.text())
+            self.listWidget.clear()
+            searchdata = searcher.search(1, self.lineEdit.text())
+            for i in range(0,searchdata.__len__()):
+                data = db.print_Movie(searchdata[i])
+                self.listWidget.addItem(data[0]['title'])
+            self.listWidget.itemClicked.connect(lambda: self.screenChangeToMovieInfo(db.getidWithTitle(self.listWidget.currentItem().text())))
+
         elif self.radioButtonActor.isChecked() :
             searcher.search(2, self.lineEdit.text())
         elif self.radioButtonDirector.isChecked() :
@@ -51,31 +58,48 @@ class WindowClass(QMainWindow,form_class ):
 #영화 상세정보 화면
 form_class_movieInfo = uic.loadUiType("movie_info.ui")[0]
 class WindowClassMovieInfo(QDialog,QWidget , form_class_movieInfo):
-    def __init__(self):
+    def __init__(self,id):      #id는 movie_id
         super().__init__()
         self.setupUi(self)
         self.show()
+        data = db.getAllDataWithId(id)              #[{'act_id': 1937}, {'act_id': 2028}....] 헝식
+        print(data)
+########################배우 데이터 처리#############################################################
+        actor_id_list = db.getACtorIdWithId(id)     #[{'act_id': 1937}, {'act_id': 2028}] 형식
+        actor_list = []
+        for i in range(0,actor_id_list.__len__()):
+            actor_list.append(db.getActorNameWithActId(actor_id_list[i]['act_id']))     #[[{'act_name': '임창정'}], [{'act_name': '하지원'}]형식
+        actor_list2 = []
+        for i in range(0,actor_list.__len__()):
+            actor_list2.append(actor_list[i][0]['act_name'])        #['임창정', '하지원', '주현'] 형식
+        for i in range(0,actor_list2.__len__()):
+            self.listWidget_actor.addItem(actor_list2[i])
+########################배우 데이터 처리#############################################################
+
+        director_id = db.getDirectorIdWithId(id)
+        print(director_id)
+        director_name = db.getDirectorNameWithDirId(director_id['dir_id'])
+        print(director_name)
+        self.textEdit_director.setText(director_name['dir_name'])
+        def screenChangeToDirectorInfo(id):
+            window_directorinfo = WindowClassDirectorInfo(id)
+            window_directorinfo.exec_()
+        dir_id = db.getDirIdwithDirName(self.textEdit_director.toPlainText())
+        # self.textEdit_director.mousePressEvent = (screenChangeToDirectorInfo(dir_id['dir_id']))
+
+
+
+
+        self.textEdit_title.setText(data[0]['title'])
+        self.textEdit_audienceRate.setText(str(data[0]['netizen_score']))
+        self.textEdit_journalRate.setText(str(data[0]['journal_score']))
+        self.textEdit_playTime.setText("상영시간: " + str(data[0]['playtime'])+"분")
+        self.textEdit_openingDate.setText("개봉: " + str(data[0]['open_date']))
+        self.textEdit_rate.setText(str(data[0]['movie_rate']))
 
         def actor_click(event):
             self.window_actorinfo = WindowClassActorInfo()
             self.window_actorinfo.exec_()
-
-        # def makeButton():
-        #
-
-        # self.textEdit_actor.setText("이동열,이준섭")
-        # actor_list = self.textEdit_actor.toPlainText().split(",")
-        # for i in range(0,actor_list.__len__()):
-        #     # app = QtWidgets.QApplication(sys.argv)
-        #     button = QtWidgets.QPushButton()
-        #     button.setGeometry(QtCore.QRect(100+i*50,220 , 150+i*50,250))
-        #     # self.button.setGeomerty(QtCore.QRect(100+i*50,220 , 150+i*50,250))
-        #     # button.setText = actor_list[i]
-        #     # self.button.clicked.connet(self.actor_click)
-        #     button.show()
-        self.textEdit_actor.mousePressEvent = actor_click
-        # self.textEdit_director.show()
-        # sys.exit(app.exec_())
 
     def screenChangeToActor(self):
         self.window_actroinfo = WindowClassActorInfo()
@@ -84,7 +108,14 @@ class WindowClassMovieInfo(QDialog,QWidget , form_class_movieInfo):
 
 form_class_actorInfo = uic.loadUiType("actor_info.ui")[0]
 class WindowClassActorInfo(QDialog,QWidget ,form_class_actorInfo):
-    def __init__(self):
+    def __init__(self,id):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+form_class_directorInfo = uic.loadUiType("director_info.ui")[0]
+class WindowClassDirectorInfo(QDialog,QWidget ,form_class_directorInfo):
+    def __init__(self,id):
         super().__init__()
         self.setupUi(self)
         self.show()
